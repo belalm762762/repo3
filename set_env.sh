804745dd41c8e963b0d899cb23781f47d1a69d4a
Mohamed Belal <mohamed.belal@uniboardfa.local>
2022-10-04 12:49:32 +0200

    Add existing file

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=mbelal	
+export POSTGRES_PASSWORD=mbelal123
+export POSTGRES_HOST=postgres.cgasqzrpmk26.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::project3-uda
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=test
+export URL=http://localhost:8100
